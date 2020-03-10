3642802b3aea528925736adabe75836fa2f3bb64
Rishabh Kharbanda <techie448@gmail.com>
2020-03-10 14:22:45 -0400

    abc

--- public/index.js
+++ public/index.js
@@ -1,3 +1,3 @@
 let p = document.createElement("p");
-p.textContent = 'this is my first app - Rishabh Kharbanda ';
+p.textContent = 'this is my first app - Rishabh Kharbanda';
 document.getElementsByTagName('body')[0].appendChild(p);
