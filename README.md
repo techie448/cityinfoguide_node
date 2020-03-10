629d41a08ba3a9090fc33ef48633d9a84bf32f4b
Rishabh Kharbanda <rishabhkharbanda@Rishabhs-MacBook-Pro.local>
2020-03-10 03:30:35 -0400

    ready to deploy

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 Edit me later
+
+`git add .
+git commit -m "ready to deploy"
+git push heroku master`
+
+**API**
+_https://dev.to/lenmorld/rest-api-with-mongodb-atlas-cloud-node-and-express-in-10-minutes-2ii1_
+
+**DEPLOY**
+_https://www.freecodecamp.org/news/how-to-deploy-your-site-using-express-and-heroku/_
+
--- app.js
+++ app.js
@@ -1,14 +1,104 @@
 // create an express app
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+
+app.use(express.json());
 
 // use the express-static middleware
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 // define the first route
 app.get("/", function (req, res) {
     res.send("<h1>Hello World!</h1>")
-})
+});
+
+
+
+const db = require("./db");
+const dbName = "data";
+const collectionName = "movies";
+
+db.initialize(dbName, collectionName, function(dbCollection) { // successCallback
+    // get all items
+    dbCollection.find().toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+
+    // << db CRUD routes >>
+
+    // create
+
+    app.post("/items", (request, response) => {
+        const item = request.body;
+        dbCollection.insertOne(item, (error, result) => { // callback of insertOne
+            if (error) throw error;
+            // return updated list
+            dbCollection.find().toArray((_error, _result) => { // callback of find
+                if (_error) throw _error;
+                response.json(_result);
+            });
+        });
+    });
+
+    // get one
+
+    app.get("/items/:id", (request, response) => {
+        const itemId = request.params.id;
+
+        dbCollection.findOne({ id: itemId }, (error, result) => {
+            if (error) throw error;
+            // return item
+            response.json(result);
+        });
+    });
+
+    // get all
+
+    app.get("/items", (request, response) => {
+        // return updated list
+        dbCollection.find().toArray((error, result) => {
+            if (error) throw error;
+            response.json(result);
+        });
+    });
+
+    // update
+
+    app.put("/items/:id", (request, response) => {
+        const itemId = request.params.id;
+        const item = request.body;
+        console.log("Editing item: ", itemId, " to be ", item);
+
+        dbCollection.updateOne({ id: itemId }, { $set: item }, (error, result) => {
+            if (error) throw error;
+            // send back entire updated list, to make sure frontend data is up-to-date
+            dbCollection.find().toArray(function(_error, _result) {
+                if (_error) throw _error;
+                response.json(_result);
+            });
+        });
+    });
+
+    // delete
+
+    app.delete("/items/:id", (request, response) => {
+        const itemId = request.params.id;
+        console.log("Delete item with id: ", itemId);
+
+        dbCollection.deleteOne({ id: itemId }, function(error, result) {
+            if (error) throw error;
+            // send back entire updated list after successful request
+            dbCollection.find().toArray(function(_error, _result) {
+                if (_error) throw _error;
+                response.json(_result);
+            });
+        });
+    });
+
+}, function(err) { // failureCallback
+    throw (err);
+});
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000,
